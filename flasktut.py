2a7328f604ede06dde44ca2aa9c50160090e9bb0
Jordan <Jordforb@gmail.com>
2022-05-05 02:52:17 -0400

    initial commit

---
+++ flasktut.py
@@ -0,0 +1,22 @@
+from flask import Flask, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "<h1>HELLO</h1> <a href=\'/Shonen Flop\'>Shonen Flop</a>"
+
+@app.route("/<name>")
+def user(name):
+    return f"Hello {name}!"
+
+@app.route("/backhome") #redirects home
+def backHome():
+    return redirect(url_for("home")) #name of method not name of route
+
+@app.route("/admin")
+def admin():
+    return redirect(url_for("user",name="Admin"))
+
+if __name__=="__main__":
+    app.run()
